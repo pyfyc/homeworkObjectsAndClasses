e1d8fc27151ed837e2180c8d48f44a0274d0d6dd
Alex <alexeym75@gmail.com>
2022-08-05 18:55:12 +0700

    Added tasks

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+    private final String title;
+    private final Author author;
+    private int publishingYear;
+
+    public Book(String title, Author author, int publishingYear) {
+        this.title = title;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        if (publishingYear < 1950 || publishingYear > 2050) {
+            System.out.println("Invalid publishingYear parameter: " + publishingYear);
+            return;
+        }
+        this.publishingYear = publishingYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Author stephenKing = new Author("Stephen", "King");
+        Author ernestHemingway = new Author("Ernest", "Hemingway");
+
+        Book christine = new Book("Christine", stephenKing, 1983);
+        Book misery = new Book("Misery", stephenKing, 1987);
+        Book islandsInTheStream = new Book("Islands In The Stream", ernestHemingway, 1900);
+
+        islandsInTheStream.setPublishingYear(1970);
+
+        Library library = new Library(5);
+
+        library.addBook(christine);
+        library.addBook(misery);
+        library.addBook(islandsInTheStream);
+
+        library.printLibrary();
+
+        library.printInfoByTitle("Misery");
+        if (library.changePulishingYear("Misery", 2000)) {
+            library.printInfoByTitle("Misery");
+        }
     }
 }
